da544520a9bb1fa8152b96382c0ed12e9e6edfda
Qianying Ruan <124208512+1239874569898@users.noreply.github.com>
2023-10-22 22:08:18 -0700

    Add files via upload

---
+++ page.js
@@ -0,0 +1,12 @@
+import items from ".item-list";
+
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold m-6 text-center text-white">
+      Shopping List
+      </h1>
+      <Itemlist />
+    </main>
+  );
+}
